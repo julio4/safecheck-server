ca0d641bc9cab8ceca457dfd729c19d006369d38
clementjuventin <clementjuventin@gmail.com>
2022-10-28 22:36:41 +0200

    feat: transaction collector

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,27 +1,25 @@
 const txRouter = require('express').Router()
 const Transaction = require('../models/transaction')
 const logger = require('../utils/logger')
+const DataCollector = require('../models/dataCollector')
 
 const { simulateTx } = require('../services/simulate')
 
-txRouter.get('/', (request, response) => {
-  response.json({
-    "status": "Success!"
-  })
+txRouter.get('/', async (request, response) => {
+  response
+    .status(200)
+    .json(dataTx.toJSON())
 })
 
 txRouter.post('/', async (request, response) => {
-  const tx = request.body
+  const dataTx = new DataCollector(request.body)
+
+  await dataTx.populateData()
 
   // validate transaction
   // TODO
   logger.info("Received tx:")
-  logger.info(tx)  
-
-  // construct insights object
-  let insights = {
-    "Status": "Received",
-  }
+  logger.info(dataTx.toJSON())
 
   // simulate tx
   
@@ -29,7 +27,7 @@ txRouter.post('/', async (request, response) => {
 
   response
     .status(200)
-    .json(insights)
+    .json(dataTx.toJSON())
 })
 
 module.exports = txRouter
