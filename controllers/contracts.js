619c7507f970df3a306570bd38c5807961e45504
quintenbons <quintenbons@outlook.com>
2022-10-30 08:10:05 +0100

    bacalhau local

--- controllers/contracts.js
+++ controllers/contracts.js
@@ -4,30 +4,32 @@ const logger = require('../utils/logger')
 const ContractDataCollector = require('../models/contractDataCollector')
 const { getContractCalls } = require('../services/eth_requests')
 const { addToIPFS } = require('../services/ipfs')
-const { getContractInfo } = require('../services/bacalhau')
+const { getContractInfo, callBacalhau, localBac } = require('../services/bacalhau')
 
 contractRouter.get('/:hash', async (request, response) => {
   const contractHash = request.params.hash
   const addressRe = /^0x[a-fA-F0-9]{40}$/;
-  if (!adddressRe.test(contractHash)) {
-    response
-      .status(400)
-      .json({
-        "error": "Address in url is incorrect"
-      });
+  // if (!addressRe.test(contractHash)) {
+  //   response
+  //     .status(400)
+  //     .json({
+  //       "error": "Address in url is incorrect"
+  //     });
+  //   return;
+  // }
+
+  // const contractInfo = await getContractInfo(contractHash);
+
+  //if (contractInfo === null) {
+    const calls = await getContractCalls(contractHash);
+    // const cid = await addToIPFS(calls, contractHash)
+    // console.log(`Added tx list to IPFS ${cid}. Launching bacalhau job.`);
+    // callBacalhau(contractHash, cid);
+    const data = await localBac(calls);
+    response.status(404).json(localBac);
     return;
-  }
-
-  const contractInfo = await getContractInfo(contractHash);
-
-  if (contractInfo !== null)
-
-  const contractCollector = new ContractDataCollector(contractHash)
-
-  await contractCollector.populateData()
-
-  const cid = await addToIPFS(calls, contractHash)
-  logger.info(`Added tx list to IPFS ${cid}`)
+  //}
+  console.log(contractInfo);
 
   response
     .status(200)
--- services/bacalhau.js
+++ services/bacalhau.js
@@ -1,11 +1,13 @@
 const fs = require('fs');
+const { spawnSync } = require('node:child_process');
+const { getFromIPFS } = require('./ipfs');
 
 const getContractInfo = async (address) => {
   try {
-    const content = JSON.parse(fs.readFileSync('../computed/contracts.json'));
+    const content = JSON.parse(fs.readFileSync('./computed/contracts.json'));
     return (content[address] !== undefined) ? content[address] : null;
   } catch (err) {
-    console.error("Could not read content from computed")
+    console.error("Could not read content from computed", err);
   }
 }
 
@@ -18,12 +20,77 @@ const isComputed = async (address) => {
   }
 }
 
-const callBacalhau = async (cid) => {
+const callBacalhau = async (address, cid) => {
+  // Launch bachalau
+  const image = "quintenbons/testlisbon:1.7";
+  console.log(image, cid);
+  const result = spawnSync('bacalhau', ['docker', 'run', '--id-only', image, '--inputs', cid]);
+  let jobId = result.stdout.toString();
+  jobId = /^[^\n]*/.exec(jobId)[0];
+  console.log("Bacalhau run done", jobId);
 
+  // Write in cache
+  const result1 = spawnSync('bacalhau', ['get', '--output-dir', './computed', jobId]);
+  console.log("Bacalhau get done");
+
+  let contracts = JSON.parse(fs.readFileSync('./computed/contracts.json'));
+  contracts[address] = {cid, jobId};
+  fs.writeFileSync('./computed/contracts.json', JSON.stringify(contracts));
+}
+
+const localBac = async (content) => {
+  let result = { contractAdress: reSearch[0], addressCallCount: {}, oldestTimeStamp: 0, newestTimeStamp: 0, timePlot: [], transactionOverTime: [], valueOverTime: [] };
+
+  try {
+      if (content.length === 0) {
+          return result;
+      }
+
+      // Plot data
+      result.oldestTimeStamp = new Date(content[0].timestamp).getTime();
+      result.newestTimeStamp = new Date(content[content.length - 1].timestamp).getTime();
+      const sturges = 1 + Math.floor(Math.log2(content.length));
+      const step = Math.floor((result.newestTimeStamp - result.oldestTimeStamp) / sturges);
+
+      let blockStart = result.oldestTimeStamp;
+      let blockCount = 0;
+      let blockValue = 0;
+      let nextStep = result.oldestTimeStamp + step;
+
+      for (let transaction of content) {
+          if (result.addressCallCount[transaction.from_address] === undefined)
+              result.addressCallCount[transaction.from_address] = 0;
+          result.addressCallCount[transaction.from_address]++;
+
+          // Pass to next block
+          const transactionTime = new Date(transaction.timestamp).getTime();
+          while (transactionTime > nextStep) {
+              result.timePlot.push(blockStart);
+              result.transactionOverTime.push(blockCount);
+              result.valueOverTime.push(blockValue);
+
+              blockStart = nextStep;
+              blockCount = 0;
+              blockValue = 0;
+
+              nextStep = blockStart + step;
+          }
+
+          blockCount++;
+          blockValue += transaction.value;
+      }
+
+  } catch (err) {
+      console.error(err);
+      return result;
+  }
+
+  return result;
 }
 
 module.exports = {
   getContractInfo,
   isComputed,
-  callBacalhau
+  callBacalhau,
+  localBac,
 };
